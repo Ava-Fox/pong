59947c182d295e86249d374752351557cbe50601
Ivy Wong <ivy@ivywong.dev>
2024-02-21 17:31:22 -0500

    fix bug with moving paddle

--- pong.py
+++ pong.py
@@ -17,10 +17,10 @@ class Paddle:
         self.game.screen.fill("white", self.rect)
 
     def update(self):
-        if self.moving_up and self.rect.top < (self.game.screen_rect.top - 50):
+        if self.moving_up and self.rect.top > self.game.wall_top.bottom:
             self.y -= self.speed
             print("moving up")
-        if self.moving_down and self.rect.bottom > (self.game.screen_rect.bottom - 50):
+        if self.moving_down and self.rect.bottom < self.game.wall_bottom.top:
             self.y += self.speed
             print("moving down")
         # Update rect object from self.x
@@ -44,17 +44,11 @@ class Pong:
         self.screen.fill("red")
         self.paddle_left = Paddle(self, 0, self.screen_rect.centery)
         self.paddle_right = Paddle(self, (self.screen_rect.right - 25), self.screen_rect.centery)
-
         self.wall_top = pygame.Rect(0, 0, self.screen_rect.width, 50)
         self.wall_bottom = pygame.Rect(0, 0, self.screen_rect.width, 50)
         self.divider = pygame.Rect(0, 0, 10, self.screen_rect.height)
         self.wall_bottom.bottom = self.screen_rect.bottom
         self.divider.center = self.screen_rect.center
-        self.screen.fill((141, 29, 5), self.divider)
-        self.screen.fill("white", self.wall_top)
-        self.screen.fill("white", self.wall_bottom)
-
-        
         
     def run_game(self):
         while self.running:
@@ -66,30 +60,37 @@ class Pong:
             pygame.display.flip()
             self.clock.tick(60)
 
+    def _draw_world(self):
+        self.screen.fill((141, 29, 5), self.divider)
+        self.screen.fill("white", self.wall_top)
+        self.screen.fill("white", self.wall_bottom)
+
     def _update_screen(self):
+        self.screen.fill("red") 
+        
+        self._draw_world()
         self.paddle_left.draw()
         self.paddle_right.draw()
-         
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-                elif event.type == pygame.KEYDOWN:
-                    self._check_keydown_events(event)
-                elif event.type == pygame.KEYUP:
-                    self._check_keyup_events(event)
+            if event.type == pygame.QUIT:
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
 
     def _check_keydown_events(self, event):
-         if event.key == pygame.K_UP:
+        if event.key == pygame.K_UP:
             self.paddle_right.moving_up = True
-         elif event.key == pygame.K_DOWN:
+        elif event.key == pygame.K_DOWN:
             self.paddle_right.moving_down = True
-         elif event.key == pygame.K_w:
-             self.paddle_left.moving_up = True
-         elif event.key == pygame.K_s:
-             self.paddle_left.moving_down = True
+        elif event.key == pygame.K_w:
+            self.paddle_left.moving_up = True
+        elif event.key == pygame.K_s:
+            self.paddle_left.moving_down = True
 
     def _check_keyup_events(self, event):
          if event.key == pygame.K_UP:
